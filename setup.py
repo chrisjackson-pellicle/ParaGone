1fe4b130a559cbcab2ffd4f316ba1953e9182393
chrisjackson <chris.jackson@rbg.vic.gov.au>
2023-01-17 18:54:16 +1100

    Various updates

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 resolve_paralogs_scripts = ['paragone']
 resolve_paralogs_description = 'Paralogy resolution using Yang and Smith algorithms'
 resolve_paralogs_url = 'https://github.com/chrisjackson-pellicle/yang_and_smith_paralogy_resolution'
-resolve_paralogs_entry_points = {'console_scripts': ['paragone = paragone_main:main']}
+resolve_paralogs_entry_points = {'console_scripts': ['paragone = paragone.paragone_main:main']}
 
 setuptools.setup(name='paragone',
                  version='0.0.1rc',
