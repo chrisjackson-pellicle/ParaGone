d05504887a373a07cf5386c1392847245eba42e7
chrisjackson <chris.jackson@rbg.vic.gov.au>
2023-03-09 10:05:08 +1100

    Update trim_trees_treeshrink.py

--- paragone/trim_trees_treeshrink.py
+++ paragone/trim_trees_treeshrink.py
@@ -12,190 +12,12 @@ import os
 import textwrap
 import glob
 import subprocess
-import re
 import shutil
 from ete3 import Tree
 
 from paragone import phylo3
 from paragone.tree_utils import *
 from paragone import utils
-
-
-# return the outlier tip, with abnormal high contrast and long branch
-def check_contrast_outlier(node0,
-                           node1,
-                           above0_branch_length,
-                           above1_branch_length,
-                           relative_cutoff,
-                           tree_name=None,
-                           logger=None):
-    """
-    Return the outlier tip, with abnormal high contrast and long branch, if present, else return None
-
-    :param phylo3.Node node0: first node for comparison
-    :param phylo3.Node node1: second node for comparison
-    :param float above0_branch_length: branch length of first node
-    :param float above1_branch_length: branch length of second node
-    :param float relative_cutoff: relative cutoff for removing tree tips
-    :param str tree_name: name of the tree e.g. 6886.paralogs.aln.hmm.trimmed.fasta.treefile
-    :param logging.Logger logger: a logger object
-    :return phylo3.Node/None: return outlier node if present, else None
-    """
-
-    if node0.istip and above0_branch_length > relative_cutoff:
-        # CJJ I DON'T UNDERSTAND THIS - WHY COMPARE TO ZERO BRANCH LENGTH: IS THIS IF KINKS HAVE BEEN INTRODUCED?
-        if above1_branch_length == 0.0:
-            return node0, f'Branch length {above0_branch_length} is above relative cut-off value of ' \
-                          f'{relative_cutoff}, and sister branch length is {above1_branch_length}'
-
-        elif above0_branch_length / above1_branch_length > 10:  # i.e. more than ten times longer than sister branch
-            return node0, f'Branch length {above0_branch_length} is above relative cut-off value of ' \
-                          f'{relative_cutoff}, and is >10 times longer than sister branch ({above1_branch_length})'
-
-    if node1.istip and above1_branch_length > relative_cutoff:
-        # CJJ I DON'T UNDERSTAND THIS - WHY COMPARE TO ZERO BRANCH LENGTH, IS THIS IF KINKS HAVE BEEN INTRODUCED?
-        if above0_branch_length == 0.0:
-            return node1, f'Branch length {above1_branch_length} is above relative cut-off value of ' \
-                          f'{relative_cutoff}, and sister branch length is {above0_branch_length}'
-
-        elif above1_branch_length / above0_branch_length > 10:
-            return node1, f'Branch length {above1_branch_length} is above relative cut-off value of ' \
-                          f'{relative_cutoff}, and is >10 times longer than sister branch ({above0_branch_length})'
-
-    return None, None
-
-
-def remove_a_tip(root,
-                 tip_node,
-                 min_tips,
-                 tree_name=None,
-                 logger=None):
-    """
-    Remove a given tip from a tree, and remove the 'kink' that this produces.
-
-    :param phylo3.Node root: tree object parsed by newick3.parse
-    :param phylo3.Node tip_node:
-    :param int min_tips: the minimum number of tips in a tree after trimming tips
-    :param str tree_name: name of the tree e.g. 6886.paralogs.aln.hmm.trimmed.fasta.treefile
-    :param logging.Logger logger: a logger object
-    :return phylo3.Node/None root/None: pruned tree if more than four tips remaining, else None
-    """
-
-    node = tip_node.prune()
-
-    if len(root.leaves()) >= min_tips:
-        node, root = remove_kink(node, root)
-        return root
-    else:
-        logger.warning(f'{"[WARNING]:":10} After removing tip {tip_node.label}, tree {tree_name} has fewer than '
-                       f'{min_tips} tips left.')
-        return None
-
-
-def trim(curroot,
-         relative_cutoff,
-         absolute_cutoff,
-         min_tips,
-         tree_name=None,
-         logger=None):
-    """
-    Removes tree tips longer than specified cutoffs
-
-    :param phylo3.Node curroot: tree object parsed by newick3.parse
-    :param float relative_cutoff: relative cutoff for removing tree tips
-    :param float absolute_cutoff: absolute cutoff for removing tree tips
-    :param int min_tips: the minimum number of tips in a tree after trimming tips
-    :param str tree_name: name of the tree e.g. 6886.paralogs.aln.hmm.trimmed.fasta.treefile
-    :param logging.Logger logger: a logger object
-    :return:
-    """
-
-    if curroot.nchildren == 2:
-        print('YEAH')
-        temp, root = remove_kink(curroot, curroot)  # CJJ not used?
-
-    going = True
-    nodes_above_absolute_cutoff = defaultdict(list)
-    nodes_above_relative_cutoff = defaultdict(list)
-
-    while going and curroot and len(curroot.leaves()) > 3:
-        going = False
-        for node in curroot.iternodes(order=1):  # POSTORDER
-
-            # Check if node is a tip, and remove it if branch length is greater than absolute cutoff:
-            if node.nchildren == 0:  # at the tip
-                node.data['len'] = node.length
-                if node.length > absolute_cutoff:
-                    logger.debug(f'Tip {node.label} is on a branch with length {node.length}. Absolute cutoff is '
-                                 f'{absolute_cutoff}. This tip will be removed.')
-                    nodes_above_absolute_cutoff[node.label].extend([node.length, absolute_cutoff])
-                    curroot = remove_a_tip(curroot,
-                                           node,
-                                           min_tips,
-                                           tree_name=tree_name,
-                                           logger=logger)
-                    going = True
-                    break
-            elif node.nchildren == 1:  # kink in tree
-                print('KINK')
-                remove_kink(node, curroot)
-                going = True
-                break
-            elif node.nchildren == 2:  # normal bifurcating internal nodes
-                child0_node, child1_node = node.children[0], node.children[1]
-                above0_branch_length, above1_branch_length = child0_node.data['len'], child1_node.data['len']
-                # CJJ internal branch length is adjusted below - why?:
-                node.data['len'] = ((above0_branch_length + above1_branch_length) / 2.) + node.length  # stepwise average
-                outlier, reason = check_contrast_outlier(child0_node,
-                                                         child1_node,
-                                                         above0_branch_length,
-                                                         above1_branch_length,
-                                                         relative_cutoff,
-                                                         tree_name=tree_name,
-                                                         logger=logger)
-                # if outlier != None:
-                if outlier:
-                    nodes_above_relative_cutoff[outlier.label].extend([reason, 'bifurcating internal node'])
-                    curroot = remove_a_tip(curroot,
-                                           outlier,
-                                           min_tips,
-                                           tree_name=tree_name,
-                                           logger=logger)
-                    going = True  # need to keep checking
-                    break
-            else:  # 3 or more branches from this node. Pair-wise comparison
-                total_len = 0
-                nchild = node.nchildren
-                for child in node.children:
-                    total_len += child.data['len']
-                node.data['len'] = total_len / float(node.nchildren)
-                keep_checking = True
-                for index1 in range(nchild):  # do all the pairwise comparison
-                    for index2 in range(nchild):
-                        if index2 <= index1:
-                            continue  # avoid repeatedly checking a pair
-                        child1_node, child2_node = node.children[index1], node.children[index2]
-                        above1_branch_length, above2_branch_length = child1_node.data['len'], child2_node.data['len']
-                        outlier, reason = check_contrast_outlier(child1_node,
-                                                                 child2_node,
-                                                                 above1_branch_length,
-                                                                 above2_branch_length,
-                                                                 relative_cutoff,
-                                                                 logger=logger)
-                        if outlier:
-                            nodes_above_relative_cutoff[outlier.label].extend([reason, '>=3 branches at node'])
-                            curroot = remove_a_tip(curroot,
-                                                   outlier,
-                                                   tree_name=tree_name,
-                                                   logger=logger)
-                            going = True  # need to keep checking
-                            keep_checking = False  # to break the nested loop
-                            break
-                    if not keep_checking:
-                        break
-
-    return curroot, nodes_above_absolute_cutoff, nodes_above_relative_cutoff
-
 
 def write_trim_report(collated_trim_report_dict,
                       report_directory,
@@ -273,7 +95,7 @@ def treeshrink(treefile,
 
     :param str treefile: name of tree file in newick format
     :param str trimmed_tree_output_folder: name of output folder for trimmed trees
-    :param float q_value: value provided the TreeShrink --quantile paramter; default is 0.05
+    :param float q_value: value provided the TreeShrink --quantile parameter; default is 0.05
     :param logging.Logger logger: a logger object
     :return str, list: expected_shrunk_treefile_renamed, tips_removed: name of (renamed and moved) tree file output
     by TreeShrink, and a list of tips removed (if any).
